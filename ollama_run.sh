a95edf965149891207aef9ee6434fbafba23cc8f
karim <kawzaknobo515151@gmail.com>
2026-05-18 07:46:22 +0300

    new a

--- ollama_run.sh
+++ ollama_run.sh
@@ -1,4 +1,5 @@
 sudo apt update -y
+sudo apt install usbutils pciutils lshw
 sudo apt-get install zstd
 sudo apt install python3
 sudo apt install python3-pip -y
@@ -6,6 +7,4 @@ apt install python3-venv python3-full -y
 sudo apt install python-is-python3
 sudo update-alternatives --install /usr/bin/pip pip /usr/bin/pip3
 curl -fsSL https://ollama.com/install.sh | sh
-python3 -m venv '/content/Sunnay_Colabs/ollama'
-source /content/Sunnay_Colabs/ollama/bin/activate
 pip3 install -r '/content/Sunnay_Colabs/ollama_requirements.txt' 
