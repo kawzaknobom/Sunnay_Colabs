32552149e9e3ba11ef432ba4c9dbb3ec6f5e01ce
karim <kawzaknobo515151@gmail.com>
2026-06-13 07:44:44 +0300

    what a good

--- blurbot.py
+++ blurbot.py
@@ -86,7 +86,7 @@ async def get_gender(frame):
 
 async def get_persons(frame):
    last_known_people = []
-   results = model.track(frame, persist=True, tracker="custom_tracker.yaml", classes=[0])
+   results = model.track(frame, persist=True, tracker="/content/Sunnay_Colabs/custom_tracker.yaml", classes=[0])
    for r in results:
          if r.boxes.id is not None:
             for box, track_id in zip(r.boxes.xyxy, r.boxes.id):
